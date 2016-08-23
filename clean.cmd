e4b7bffae0bef08dc8f556a9fc7e0d508699ed20
Jan Pospisil <j.pospisil@cern.ch>
2016-08-23 11:54:33 +0200

    added SyncTeX support

--- clean.cmd
+++ clean.cmd
@@ -2,6 +2,8 @@ REM del *.pdf
 
 del *.aux *.toc *.log *.out *.dvi
 
+del *.synctex*
+
 del .*.xml ._* .DS_Store
 
 del *.tex~ *.sty~
--- compile.cmd
+++ compile.cmd
@@ -1,5 +1,7 @@
 REM compile twice and rename
-pdflatex ./master.tex
-pdflatex ./master.tex
+pdflatex -synctex=1 ./master.tex
+pdflatex -synctex=1 ./master.tex
 copy master.pdf free_range_vhdl.pdf
+copy master.synctex.gz free_range_vhdl.synctex.gz
 del master.pdf
+del master.synctex.gz
