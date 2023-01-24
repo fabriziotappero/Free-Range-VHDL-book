c772a0e3a0e05cf445b4d393d805edb70ef5891d
Fabrizio <fabrizio@PF408F5W.itk.local>
2023-01-24 12:03:35 +0100

    fixed script rm command

--- clean.cmd
+++ clean.cmd
@@ -1,14 +1,8 @@
 REM del *.pdf
-
-del *.aux *.toc *.log *.out *.dvi
-
-del *.synctex*
-
-del .*.xml ._* .DS_Store
-
-del *.tex~ *.sty~
-
-del -R __MACOSX
-
-del *~*
+del ./*.aux ./*.toc ./*.log ./*.out ./*.dvi
+del ./*.synctex*
+del ./.*.xml ._* ./.DS_Store
+del ./*.tex~ ./*.sty~
+del -R ./__MACOSX
+REM del ./*~*
 
